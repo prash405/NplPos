753b0bfae5b2f2d59180f2c38d3e32dbd8f5dac0
PrashDev <pprash405@gmail.com>
2025-04-20 10:56:04 +0530

    Linked project with NplPos repo and added POS tagger

--- pos_tagger.py
+++ pos_tagger.py
@@ -1,30 +1,30 @@
-# Install required libraries first
-# pip install indic-nlp-library
-# pip install nltk
-
+import os
 from indicnlp.tokenize import indic_tokenize
 from indicnlp import loader
 import nltk
 from nltk.tag import tnt
 from nltk.corpus import indian
 
+# ✅ Set correct path to the resource folder
+INDIC_NLP_RESOURCES = "F:/devops/nlp/pos_indian_nlp_project/indic_nlp_resources-master"
+os.environ["INDIC_RESOURCES_PATH"] = INDIC_NLP_RESOURCES
+
 # Load Indic NLP resources
-INDIC_NLP_RESOURCES = './indic_nlp_resources'  # adjust path if needed
 loader.load()
 
-# Load Hindi corpus from NLTK (or use your own tagged corpus)
+# Load Hindi POS-tagged corpus
 nltk.download('indian')
 nltk.download('punkt')
 hindi_text = indian.tagged_sents('hindi.pos')
 
-# Train a POS tagger
+# Train the tagger
 tnt_pos_tagger = tnt.TnT()
 tnt_pos_tagger.train(hindi_text)
 
-# Sample Hindi sentence
+# Sample test
 test_sentence = "राम बाजार गया।"
 tokens = list(indic_tokenize.trivial_tokenize(test_sentence, lang='hi'))
 
-# Predict POS tags
+# Get POS tags
 tags = tnt_pos_tagger.tag(tokens)
 print("POS Tags:", tags)
